6aa4d6df88232bbabbb318ebceeb93c7170a3efb
a4m123 <57316085+a4m123@users.noreply.github.com>
2022-05-26 15:14:23 +0700

    0

--- USB2/RMaster.py
+++ USB2/RMaster.py
@@ -6,7 +6,7 @@ resultCloseValve = 0
 
 resultWaterLeakage = RListenWater.main()
 if (resultWaterLeakage == 1):
-    resultCloseValve = RCloseValve.main()
+    resultCloseValve = RCloseValve.main(0)
     resultSendMail = RSendEMail.main(resultCloseValve)
     information = {'waterLeakage': resultWaterLeakage, 'closedValve': resultCloseValve, 'sendedMail': resultSendMail}
     infoFile = open("info.json", "w")
