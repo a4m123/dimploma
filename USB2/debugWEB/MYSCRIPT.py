5b9c4ab68040d27a54090b85c7948de7ef1a9ef3
a4m123 <57316085+a4m123@users.noreply.github.com>
2022-06-05 14:12:54 +0700

    5 june debugWeb

--- USB2/debugWEB/MYSCRIPT.py
+++ USB2/debugWEB/MYSCRIPT.py
@@ -1,6 +1,29 @@
-import json
-import os
+from flask import Flask, render_template, Response
+import cv2
+import time
 
-information = {'waterLeakage': 1, 'closedValve': 2, 'sendedMail': 3}
-infoFile = open("info24.json", "w")
-json.dump(information, infoFile)
+app = Flask(__name__)
+
+def gen_frames():  
+    cap = cv2.VideoCapture(0)
+    while(cap.isOpened()):
+        ret, img = cap.read()
+        if ret == True:
+            img = cv2.resize(img, (0,0), fx=2, fy=2) 
+            frame = cv2.imencode('.jpg', img)[1].tobytes()
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            time.sleep(0.1)
+        else: 
+            break
+            
+@app.route('/')
+def index():
+    return render_template('index.html')      
+             
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
